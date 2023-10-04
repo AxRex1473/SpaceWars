2d48295dd2b8ec9e496671596028ff4689e0cfa7
Axel Hernandez <ianaxelh@gmail.com>
2023-10-04 00:31:35 -0600

    PlayerController, Mats, Camera

---
+++ SpaceWars/Assets/Script/PlayerController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed;
+    public float rotationSpeed;
+    public Rigidbody rb;
+
+    private float horiazontalMove;
+    private float verticalMove;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horiazontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+        rb.velocity = (transform.forward * verticalMove) * playerSpeed * Time.deltaTime;
+        transform.Rotate((transform.up * horiazontalMove) * rotationSpeed * Time.deltaTime);
+    }
+}
